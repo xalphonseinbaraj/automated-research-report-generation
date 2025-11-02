dd12191c5f174cd0bf205c9cb841bfbfb4d9d41d
Alphonse <xalphonse@gmail.com>
2025-11-02 17:25:42 +0800

    test notebook updated

--- research_and_analyst/utils/model_loader.py
+++ research_and_analyst/utils/model_loader.py
@@ -3,12 +3,12 @@ import sys
 import json
 import asyncio
 from dotenv import load_dotenv
-from utils.config_loader import load_config
+from research_and_analyst.utils.config_loader import load_config
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_openai import ChatOpenAI
 from langchain_groq import ChatGroq
-from logger import GLOBAL_LOGGER as log
-from exception.custom_exception import ResearchAnalystException
+from research_and_analyst.logger import GLOBAL_LOGGER as log
+from research_and_analyst.exception.custom_exception import ResearchAnalystException
 
 
 class ApiKeyManager:
